20b8f2f47e093a9d9b258455246d35039a53e09d
Justin Tulloss <justin@harmonize.fm>
2010-01-25 20:53:29 -0800

    Unnested tests for the beauty of the source code

--- test/test.js
+++ test/test.js
@@ -10,37 +10,66 @@ var client = new aeromq.AeroMqClient();
 client.addListener('connect', function() {
     var queue = 'test';
     sys.puts("+ Successfully connected to the server!");
-    client.purge(queue);
-    client.publish(queue, 'An important message!').addCallback(function() {
-        sys.puts("+ Successfully published a message!");
-        client.monitor(queue).addCallback(function(messages) {
-            sys.puts("+ Snapshot of trial: " + sys.inspect(messages));
+    var tests = [
+        function testPublish(done) {
+            client.publish(queue, 'An important message!').addCallback(function() {
+                sys.puts("+ Successfully published a message!");
+                done();
+            });
+        },
+        function testMonitor(done) {
+            client.monitor(queue).addCallback(function(messages) {
+                sys.puts("+ Snapshot of trial: " + sys.inspect(messages));
 
-            assert.equal(messages.length, 1,
-                "After 1 publish, not 1 message on the queue");
-
+                assert.equal(messages.length, 1,
+                    "After 1 publish, not 1 message on the queue");
+                done();
+            });
+        },
+        function testSubscribe(done) {
             client.subscribe(queue).addCallback(function(message) {
                 sys.puts("+ Got a message from the server: " + message);
-                client.publish(queue, 'unimporant').addCallback(function() {
-                    sys.puts("+ Successfully published a message!");
-                });
+                done();
+            })
+        },
+        function publishUnimportant(done) {
+            client.publish(queue, 'unimporant').addCallback(function() {
+                sys.puts("+ Successfully published a message!");
                 client.publish(queue, 'slightly relevant').addCallback(function() {
                     sys.puts("+ Successfully published a message!");
-                    client.monitor(queue).addCallback(function(messages) {
-                        assert.ok(messages, "Messages should be a list");
-                        assert.equal(messages.length, 2,
-                            "Queue should have 2 messages");
-                        client.purge(queue).addCallback(function() {
-                            sys.puts("+ Successfully purged the queue");
-                            client.monitor(queue).addCallback(function(messages) {
-                                assert.equal(messages, null,
-                                    "Queue should be empty");
-                            });
-                        });
-                    });
+                    done();
                 });
             });
-        });
+        },
+        function testMonitor2(done) {
+            client.monitor(queue).addCallback(function(messages) {
+                assert.ok(messages, "Messages should be a list");
+                assert.equal(messages.length, 2,
+                    "Queue should have 2 messages");
+                done();
+            });
+        },
+        function testPurge(done) {
+            client.purge(queue).addCallback(function() {
+                sys.puts("+ Successfully purged the queue");
+                client.monitor(queue).addCallback(function(messages) {
+                    assert.equal(messages, null,
+                        "Queue should be empty");
+                    done();
+                });
+            });
+        }
+    ];
+    // Ok, we've written all the tests, now let's call them
+    var testIndex = 0;
+    function callNextTest() {
+        if (tests.length > testIndex) {
+            tests[testIndex](callNextTest);
+            testIndex++;
+        }
+    }
+    client.purge(queue).addCallback(function() {;
+        callNextTest();
     });
 });
 
